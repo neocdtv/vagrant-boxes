c32e7f60bce8eb2a279157e8a8523dd5b3c3ce29
neocdtv <krzysztof.wolf.1980@gmail.com>
2020-06-04 15:01:54 +0200

    extract scripts

---
+++ scripts/setup_vbox_additions.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+USER_HOME=$1
+VBOX_VERSION=$2
+mkdir -p $USER_HOME/Downloads
+cd $USER_HOME/Downloads        	    
+apt install -y libaio1 libaio-dev build-essential dkms linux-headers-$(uname -r) gcc make perl
+curl --progress-bar -o VBoxGuestAdditions.iso  https://download.virtualbox.org/virtualbox/$VBOX_VERSION/VBoxGuestAdditions_$VBOX_VERSION.iso
+mkdir -p /media/cdrom
+mount -o loop ./VBoxGuestAdditions.iso /media/cdrom
+cd /media/cdrom
+printf yes\\n | ./VBoxLinuxAdditions.run   
+cd $USER_HOME
