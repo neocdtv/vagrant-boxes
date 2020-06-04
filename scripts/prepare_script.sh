aa8b4ce63e0a5c0193fb462e68368d2fdad92d0a
Krzysztof Wolf <krzysztof.wolf.1980@gmail.com>
2020-06-04 23:30:41 +0200

    Update prepare_script.sh

--- scripts/prepare_script.sh
+++ scripts/prepare_script.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-cd $USER_HOME/scripts
+SCRIPT_NAME=$1
+cd $SCRIPTS_DIR
 curl -O https://raw.githubusercontent.com/neocdtv/vagrant-boxes/master/scripts/$SCRIPT_NAME
 chmod a+x $SCRIPT_NAME
