fb42801de6b561c00f4cc34e6cff0ab16f4eed15
Krzysztof Wolf <krzysztof.wolf.1980@gmail.com>
2020-06-07 19:11:07 +0200

    Update setup_jdk_11_openj9.sh

--- scripts/setup_jdk_11_openj9.sh
+++ scripts/setup_jdk_11_openj9.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 mkdir -p $USER_HOME/Apps/jdks/openj9/adaptopenjdk
 cd $USER_HOME/Apps/jdks/openj9/adaptopenjdk
-curl -L -O https://github.com/AdoptOpenJDK/openjdk11-binaries/releases/download/jdk-11.0.7%2B10_openj9-0.20.0/OpenJDK11U-jdk_x64_linux_openj9_11.0.7_10_openj9-0.20.0.tar.gz
+curl --progress-bar -L -O https://github.com/AdoptOpenJDK/openjdk11-binaries/releases/download/jdk-11.0.7%2B10_openj9-0.20.0/OpenJDK11U-jdk_x64_linux_openj9_11.0.7_10_openj9-0.20.0.tar.gz
 tar -xzvf OpenJDK11U-jdk_x64_linux_openj9_11.0.7_10_openj9-0.20.0.tar.gz
 rm OpenJDK11U-jdk_x64_linux_openj9_11.0.7_10_openj9-0.20.0.tar.gz
 cd $USER_HOME
