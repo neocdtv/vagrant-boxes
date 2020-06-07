5303d6a2a15bb4a46d0aa5728feb4798d619597e
neocdtv <krzysztof.wolf.1980@gmail.com>
2020-06-08 00:46:20 +0200

    add cds

--- scripts/setup_jdk_11_hotspot.sh
+++ scripts/setup_jdk_11_hotspot.sh
@@ -5,6 +5,7 @@ cd $JDK_ZULU
 curl --progress-bar -L -O https://cdn.azul.com/zulu/bin/zulu11.39.15-ca-jdk11.0.7-linux_x64.tar.gz
 tar -xzvf zulu11.39.15-ca-jdk11.0.7-linux_x64.tar.gz
 rm zulu11.39.15-ca-jdk11.0.7-linux_x64.tar.gz
+chmod -R a+rwx zulu11.39.15-ca-jdk11.0.7-linux_x64
 
 echo "" >> $USER_HOME/.bashrc
 echo 'export MAVEN_OPTS_HOTSPOT="-Xnoclassgc -Xms1g -Xmx2g -XX:+TieredCompilation -XX:TieredStopAtLevel=1 -XX:+UseParallelGC -Xverify:none -Xshare:on"' >> $USER_HOME/.bashrc
