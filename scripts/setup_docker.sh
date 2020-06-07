dc2d99b8ad6ead516ad9dd313a1dc286255af929
neocdtv <krzysztof.wolf.1980@gmail.com>
2020-06-07 19:04:50 +0200

    update docker jdk hotspot

--- scripts/setup_docker.sh
+++ scripts/setup_docker.sh
@@ -4,7 +4,8 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $DISTRIB_CODENAME stable"
 apt-get update
 apt-get install -y docker-ce
-     
+echo "{\"experimental\": true}" >> /etc/docker/daemon.json
+
 curl -L https://github.com/docker/compose/releases/download/1.26.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 
--- scripts/setup_jdk_11_hotspot.sh
+++ scripts/setup_jdk_11_hotspot.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
-apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 0xB1998361219BD9C9
-apt-add-repository 'deb http://repos.azulsystems.com/ubuntu stable main'
-apt-get update
-apt-get install zulu-11
+JDK_ZULU=$USER_HOME/Apps/jdks/hotspot/zulu
+mkdir -p $JDK_ZULU
+cd $JDK_ZULU
+curl -L -O https://cdn.azul.com/zulu/bin/zulu11.39.15-ca-jdk11.0.7-linux_x64.tar.gz
+tar -xzvf zulu11.39.15-ca-jdk11.0.7-linux_x64.tar.gz
+rm zulu11.39.15-ca-jdk11.0.7-linux_x64.tar.gz
+
+echo "" >> $USER_HOME/.bashrc
+echo "jdk_11_hotspot() {" >> $USER_HOME/.bashrc
+echo '	export JAVA_HOME=$JDK_ZULU/zulu11.39.15-ca-jdk11.0.7-linux_x64'  >> $USER_HOME/.bashrc
+echo '	export PATH="$JAVA_HOME/bin:$PATH"'  >> $USER_HOME/.bashrc
+# add M2_HOME
+echo "}" >> $USER_HOME/.bashrc
+echo "" >> $USER_HOME/.bashrc
+# configure zulu 11 as default jdk
+echo "jdk_11_hotspot" >> $USER_HOME/.bashrc
+
+cd $USER_HOME
