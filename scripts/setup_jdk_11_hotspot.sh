43baa6f8b16be131aa4e3eac7070327ef952cbda
neocdtv <krzysztof.wolf.1980@gmail.com>
2020-06-07 14:35:52 +0200

    add jdk 11 setups

---
+++ scripts/setup_jdk_11_hotspot.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 0xB1998361219BD9C9
+apt-add-repository 'deb http://repos.azulsystems.com/ubuntu stable main'
+apt-get update
+apt-get install zulu-11
