c34cbd0bd2af86fd2f3cac37a1c2a1048d88e596
neocdtv <krzysztof.wolf.1980@gmail.com>
2020-06-08 14:52:04 +0200

    simplify

--- scripts/prepare_script.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-SCRIPT_NAME=$1
-cd $SCRIPTS_DIR
-curl -O https://raw.githubusercontent.com/neocdtv/vagrant-boxes/master/scripts/$SCRIPT_NAME
-chmod a+x $SCRIPT_NAME
-./$SCRIPT_NAME
-cd $USER_HOME
