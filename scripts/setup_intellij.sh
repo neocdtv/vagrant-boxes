d11fcf02b24e578153d1f2c3de84627884e84f31
neocdtv <krzysztof.wolf.1980@gmail.com>
2020-06-11 17:43:32 +0200

    fix intellij

--- scripts/setup_intellij.sh
+++ scripts/setup_intellij.sh
@@ -7,15 +7,15 @@ tar -xzvf ideaIC-2020.1.2.tar.gz
 rm ideaIC-2020.1.2.tar.gz
 chmod -R a+rwx idea-IC-201.7846.76
 
-echo '#!/usr/bin/env xdg-open' >> Intellij.desktop
-echo '[Desktop Entry]' >> Intellij.desktop
-echo 'Version=1.0' >> Intellij.desktop
-echo 'Type=Application' >> Intellij.desktop
-echo 'Terminal=false' >> Intellij.desktop
-echo "Icon=$INTELLIJ_DIR/idea-IC-201.7846.76/bin/idea.svg" >> Intellij.desktop
-echo "Icon[en_US]=$INTELLIJ_DIR/idea-IC-201.7846.76/bin/idea.svg" >> Intellij.desktop
-echo 'Name[en_US]=Intellij' >> Intellij.desktop
-echo "Exec=$INTELLIJ_DIR/idea-IC-201.7846.76/bin/idea.sh" >> Intellij.desktop
-echo 'Name=Intellij' >> Intellij.desktop
+echo '#!/usr/bin/env xdg-open' >> $USER_HOME/Desktop/Intellij.desktop
+echo '[Desktop Entry]' >> $USER_HOME/Desktop/Intellij.desktop
+echo 'Version=1.0' >> $USER_HOME/Desktop/Intellij.desktop
+echo 'Type=Application' >> $USER_HOME/Desktop/Intellij.desktop
+echo 'Terminal=false' >> $USER_HOME/Desktop/Intellij.desktop
+echo "Icon=$INTELLIJ_DIR/idea-IC-201.7846.76/bin/idea.svg" >> $USER_HOME/Desktop/Intellij.desktop
+echo "Icon[en_US]=$INTELLIJ_DIR/idea-IC-201.7846.76/bin/idea.svg" >> $USER_HOME/Desktop/Intellij.desktop
+echo 'Name[en_US]=Intellij' >> $USER_HOME/Desktop/Intellij.desktop
+echo "Exec=$INTELLIJ_DIR/idea-IC-201.7846.76/bin/idea.sh" >> $USER_HOME/Desktop/Intellij.desktop
+echo 'Name=Intellij' >> $USER_HOME/Desktop/Intellij.desktop
 
-chmod a+rwx Intellij.desktop
+chmod a+rwx $USER_HOME/Desktop/Intellij.desktop
